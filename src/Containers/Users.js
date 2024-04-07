edd36b9d30266d754eed464eb7bc13fd1c43be9a
thakurpranjalraghav <raghavthakurpranjal@gmail.com>
2024-04-07 15:33:27 +0530

    second commit

--- src/Containers/Users.js
+++ src/Containers/Users.js
@@ -1,16 +1,22 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 function Users() {
     const [users, setUsers] = useState([
-        {name: 'Pranjal', age:20},
-        {name:'Raghav', age:20}
     ])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            setUsers(data)
+        })
+    })
+    
     return(
         <div>
             <h2>Users</h2>
             <ul>
                 {
                     users.map((user,index) => {
-                        return <li>{user.name},{user.age}</li>
+                        return <li>{user.name},{user.email}</li>
                     })
                 }
             </ul>
